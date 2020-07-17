f7b039e2b52fb778ea41995dd26285b9e68226b7
Toni Peise <toni.peise@gmx.net>
2020-07-17 18:07:09 +0200

    Pageination

--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
